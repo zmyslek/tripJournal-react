88a30e7b9b44455cface8d7b9dd5192ced7b60a4
unknown <zuzia.myslek@gmail.com>
2026-04-03 23:43:12 +0200

    layout and map improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import Map from "./Map";
+
 function App() {
-    return <div><Map /></div>;
+    return (
+        <>
+            <nav className="bg-[#7A3F00] py-4 text-center">
+                <h1 className="font-adamina text-4x text-[#FFEAD4]">TripJournal</h1>
+            </nav>
+            <div className="flex justify-center items-center py-6">
+                <Map />
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/Map.tsx
+++ src/Map.tsx
@@ -5,7 +5,7 @@ import "@maptiler/sdk/dist/maptiler-sdk.css";
 const STYLE_URL =
   "https://api.maptiler.com/maps/0196a729-51f8-7a04-8b3a-22b8d925ea1b/style.json?key=FelxstvCdS6k0g9YnLdK";
 
-const highlightedCountries = ["Spain", "France", "Germany"];
+// const highlightedCountries = ["Spain", "France", "Germany"];
 
 const Map: React.FC = () => {
   const mapContainer = useRef<HTMLDivElement | null>(null);
@@ -20,48 +20,17 @@ const Map: React.FC = () => {
       projection: "globe",
       center: [0, 20],
       zoom: 1.2,
-      pitch: 20
+      pitch: 20,
+      navigationControl: false,
+      geolocateControl: false,
+      scaleControl: false,
+      terrainControl: false,
+      fullscreenControl: false,
+      maptilerLogo: false,
+      forceNoAttributionControl: true
     });
 
     mapRef.current = map;
-
-    // map.on("load", async () => {
-    //   const response = await fetch("/countries.geojson");
-    //   // const countriesData = await response.json();
-
-    //   // map.addSource("countries", {
-    //   //   type: "geojson",
-    //   //   data: countriesData
-    //   // });
-
-    // //   map.addLayer({
-    // //     id: "countries-fill",
-    // //     type: "fill",
-    // //     source: "countries",
-    // //     paint: {
-    // //       "fill-color": [
-    // //         "match",
-    // //         ["get", "name"],
-    // //         ...highlightedCountries.flatMap((country) => [
-    // //           country,
-    // //           "#4f46e5"
-    // //         ]),
-    // //         "#d1d5db"
-    // //       ],
-    // //       "fill-opacity": 0.8
-    // //     }
-    // //   });
-
-    //   map.addLayer({
-    //     id: "countries-border",
-    //     type: "line",
-    //     source: "countries",
-    //     paint: {
-    //       "line-color": "#ffffff",
-    //       "line-width": 0.8
-    //     }
-    //   });
-    // });
 
     return () => {
       map.remove();
@@ -72,8 +41,8 @@ const Map: React.FC = () => {
     <div
       ref={mapContainer}
       style={{
-        width: "100%",
-        height: "100vh"
+        width: "70vh",
+        height: "70vh"
       }}
     />
   );
